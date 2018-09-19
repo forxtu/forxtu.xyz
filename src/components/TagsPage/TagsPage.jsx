02b5eb80479ac903202554518dbfbaf7a86ec4ef
forxtu <forxtu@gmail.com>
2018-09-19 12:27:32 +0200

    feat(tags): add page

--- src/components/TagsPage/TagsPage.jsx
+++ src/components/TagsPage/TagsPage.jsx
@@ -1,21 +1,48 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { uniq } from 'ramda'
+
 import { pluralise } from '../helpers/formatting'
 import { tagsPageMetadata, tagsResourceTitle } from '../helpers/siteMetadata'
+import {
+  markdownItemNodes,
+  frontmatterCategory,
+  fieldsTags
+} from '../helpers/markdown'
+import renderListItems from '../helpers/renderListItems'
+import { tagPath } from '../../build/utils/url'
+
 import Metadata from '../shared/Metadata'
 import Page from '../shared/Page/Page'
 import TagList from '../shared/TagList'
+import TextLink from '../shared/links/TextLink'
 
-const TagsPage = ({ data, pageContext }) => (
-  <Page title={pluralise(tagsResourceTitle(data))}>
-    <Metadata {...tagsPageMetadata(data)} />
-    <TagList tags={pageContext.tags} />
-  </Page>
-)
+const TagsPage = ({ data }) => {
+  const { group: allTags } = data.allMarkdownRemark
+  const { tags: tagsMetadata } = data.site.siteMetadata.metadata
+  return (
+    <Page title={pluralise(tagsResourceTitle(data))}>
+      <Metadata {...tagsMetadata} />
+      {/* <TagList tags={tags} /> */}
+      <ul>
+        {allTags.map(
+          tag =>
+            tag.fieldValue !== `null` && tag.fieldValue !== `project` ? (
+              <li key={tag.fieldValue}>
+                <TextLink to={tagPath(tag.fieldValue)}>
+                  {tag.fieldValue} ({tag.totalCount})
+                </TextLink>
+              </li>
+            ) : null
+        )}
+      </ul>
+    </Page>
+  )
+}
 
 TagsPage.propTypes = {
   data: PropTypes.object.isRequired,
-  pageContext: PropTypes.object.isRequired,
+  pageContext: PropTypes.object.isRequired
 }
 
 export default TagsPage
---
+++ src/components/pages/tags.jsx
@@ -0,0 +1,37 @@
+import { graphql } from 'gatsby'
+import TagsPage from '../TagsPage'
+
+export default TagsPage
+
+// Note: Ideally this would be in its own file and imported from a Page
+// Container, but Gatsby preprocesses the pages looking for a graphql call.
+// Moving it anywhere else results in an error.
+export const query = graphql`
+  query TagsPageQuery {
+    site {
+      siteMetadata {
+        metadata {
+          tags {
+            description
+            keywords
+            title
+          }
+        }
+        structure {
+          resources {
+            tags {
+              title
+              path
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___keywords) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
