8b82e310d1266973b1f87dc5cfd53081cc6ad85a
forxtu <forxtu@gmail.com>
2018-09-24 10:33:41 +0200

    feat(styles): btn, link, flex helpers

--- src/components/shared/links/ButtonLink.js
+++ src/components/shared/links/ButtonLink.js
@@ -4,13 +4,25 @@ import linkProps from '../../styles/mixins/linkProps'
 import BaseLink from './BaseLink'
 
 const Button = styled(BaseLink)`
-  ${linkProps(`c:textInverted`, `c:highlight`)};
-  font-weight: normal;
-  display: block;
-  ${api({
-    color: `c:textInverted`,
-    background: `g:backgroundInverted`,
-  })};
+  ${linkProps(`c:black`, `c:white`)};
+  
+  padding: 8px 15px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  outline: none;
+  margin: 0;
+
+  :hover {
+    background-color: #2EE59D;
+    color: #fff !important;
+    box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+    transform: translateY(-7px);
+  }
 `
 
 export default Button
--- src/components/shared/links/TextLink.jsx
+++ src/components/shared/links/TextLink.jsx
@@ -8,31 +8,34 @@ import BaseLink from './BaseLink'
 // Filter out styled-component-specific props so they don't trigger Unknown Prop
 // Warnings on wrapped React components
 // eslint-disable-next-line no-unused-vars
+// const TextLink = styled(({ underlineType, ...rest }) => <BaseLink {...rest} />)`
+//   ${linkProps(`c:text`, `c:white`)};
+//   position: relative;
+//   ::before {
+//     content: '';
+//     position: absolute;
+//     transition: all 0.25s ease-in-out;
+
+//     ${api({
+//       // height: `0.3em`,
+//       height: `100%`,
+//       width: 0,
+//       bottom: 0,
+//       left: 0,
+//       right: 0,
+//       background: `c:highlight`,
+//       // opacity: 0.5,
+//       zIndex: -1
+//     })};
+//   }
+
+//   html.feat-no-touchevents &:hover::before {
+//     width: 100%;
+//     transition: all 0.25s ease-in-out;
+//   }
+// `
 const TextLink = styled(({ underlineType, ...rest }) => <BaseLink {...rest} />)`
-  ${linkProps(`c:text`, `c:white`)};
-  position: relative;
-  ::before {
-    content: '';
-    position: absolute;
-    transition: all 0.25s ease-in-out;
-
-    ${api({
-      // height: `0.3em`,
-      height: `100%`,
-      width: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      background: `c:highlight`,
-      // opacity: 0.5,
-      zIndex: -1
-    })};
-  }
-
-  html.feat-no-touchevents &:hover::before {
-    width: 100%;
-    transition: all 0.25s ease-in-out;
-  }
+  ${linkProps(`c:text`, `c:highlight`)};
 `
 
 TextLink.propTypes = {
