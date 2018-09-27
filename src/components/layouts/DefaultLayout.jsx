4c25c7faa7a1c5fb92c01b1f982e9a6efca51ea7
forxtu <forxtu@gmail.com>
2018-09-27 19:58:09 +0200

    feat(DefaultLayout): add theme support by redux

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -1,90 +1,31 @@
 import { graphql, StaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { connect, Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
-import { Provider } from 'react-redux'
 
 import configureStore from '../../state'
 
 import Site from '../Site'
 import '../styles/global.css'
-import '../styles/prism-greyscale-theme.css'
+import '../styles/prism-atom-dark.css'
 import theme from '../styles/theme'
 import darkTheme from '../styles/darkTheme'
 import { setCurrentTheme } from './../../state/ducks/global/actions'
 
 const reduxStore = configureStore()
 
-// const DefaultLayout = ({ children }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query MainLayoutQuery {
-//         allMarkdownRemark(filter: { fields: { type: { eq: "article" } } }) {
-//           categoriesGroup: group(field: frontmatter___category) {
-//             fieldValue
-//             totalCount
-//           }
-//           keywordsGroup: group(field: frontmatter___keywords) {
-//             fieldValue
-//             totalCount
-//           }
-//         }
-//         site {
-//           siteMetadata {
-//             data {
-//               owner
-//               title
-//               startYear
-//               showCredit
-//             }
-//             structure {
-//               resources {
-//                 projects {
-//                   title
-//                   navTitle
-//                   path
-//                 }
-//                 articles {
-//                   title
-//                   navTitle
-//                   path
-//                 }
-//               }
-//               pages {
-//                 about {
-//                   title
-//                   navTitle
-//                   path
-//                 }
-//                 home {
-//                   title
-//                   navTitle
-//                   path
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `}
-//     render={data => (
-//       <Provider store={reduxStore}>
-//         <ThemeProvider theme={theme}>
-//           <Site data={data}>{children}</Site>
-//         </ThemeProvider>
-//       </Provider>
-//     )}
-//   />
-// )
+const { body } = document
 
-// @connect(mapStateToProps, mapDispatchToProps)
 class DefaultLayout extends Component {
   render() {
-    const { data, children } = this.props
+    const { data, children, currentTheme } = this.props
+    currentTheme === theme
+      ? (body.style.background = `#fff`)
+      : (body.style.background = `#1F1F1F`)
     return (
       <Provider store={reduxStore}>
-        <ThemeProvider theme={this.state.currentTheme}>
+        <ThemeProvider theme={currentTheme}>
           <Site data={data}>{children}</Site>
         </ThemeProvider>
       </Provider>
@@ -144,12 +85,9 @@ export default props => (
         }
       }
     `}
-    render={data =>
-      connect(
-        mapStateToProps,
-        mapDispatchToProps
-      )(<DefaultLayout data={data} {...props} />)
-    }
+    render={data => (
+      <ConnectedDefaultLayout store={reduxStore} data={data} {...props} />
+    )}
   />
 )
 
@@ -166,5 +104,10 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = {
   setCurrentTheme
 }
+
+const ConnectedDefaultLayout = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DefaultLayout)
 
 // export default DefaultLayout
