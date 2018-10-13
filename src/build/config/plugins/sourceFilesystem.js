3d39dc4a984a674d498579485a1ec4a706b6a56c
forxtu <forxtu@gmail.com>
2018-10-13 12:44:52 +0200

    style(build): format config sourceFilesystem

--- src/build/config/plugins/sourceFilesystem.js
+++ src/build/config/plugins/sourceFilesystem.js
@@ -2,6 +2,6 @@ module.exports = dir => ({
   resolve: `gatsby-source-filesystem`,
   options: {
     name: `src`,
-    path: `${process.cwd()}/src/content/${dir}`,
-  },
+    path: `${process.cwd()}/src/content/${dir}`
+  }
 })
